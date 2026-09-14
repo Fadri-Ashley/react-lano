ada19597b2b2b2bb290b860477ec12bdb9824a63
Fadri <fadriasliarga@gmail.com>
2026-09-14 09:33:06 +0700

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+function App() {
+
+  const name = "Fadri"
+  const age = 19
+
+  return (
+    <div>
+      <h1>Hello my name is {name}</h1>
+      <p>I am {age} years old</p>
+      <p>Next year my age is {age + 1}</p>
+    </div>
+  );
+}
+
+export default App;
