8397575e30f619cb9f12e3cf0c03d9e92e238583
Fadri <fadriasliarga@gmail.com>
2026-09-14 09:51:26 +0700

    learn JSX function

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,21 @@
 function App() {
 
   const name = "Fadri"
-  const age = 19
+  const city = "Argatha"
+  const hobbies = ["Playing Guitar", "Reading Book", "Playing Games"]
 
   return (
-    <div>
+    <div className="profil">
       <h1>Hello my name is {name}</h1>
-      <p>I am {age} years old</p>
-      <p>Next year my age is {age + 1}</p>
+      <p>I'm from {city}</p>
+      <p>My favorite hobby is {hobbies[2]}</p>
+      <p>Distance from Argatha to Rohngawi is {meterConvert(17)}m</p>
     </div>
   );
+}
+
+function meterConvert(km) {
+  return km * 1000
 }
 
 export default App;
