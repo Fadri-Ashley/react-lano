6be8abc95cfb1ab48f574439006af52eede9a7b4
Fadri <fadriasliarga@gmail.com>
2026-09-14 15:47:50 +0700

    learn attributes

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,38 @@
 function App() {
+  const name = "Fadri";
+  const city = "Argatha";
+  const hobbies = ["Playing Guitar", "Reading Book", "Playing Games"];
+  
+  const mystyles = {
+    color: "red",
+    fontSize: "20px",
+    backgroundColor: "lightyellow",
+  };
 
-  const name = "Fadri"
-  const city = "Argatha"
-  const hobbies = ["Playing Guitar", "Reading Book", "Playing Games"]
+  const x = 10;
+  let y = "Banana"
+  if (x < 5) {
+    y = "Apple"
+  }
+
+  const myfunc = () => {
+    alert(y);
+  };
 
   return (
     <div className="profil">
-      <h1>Hello my name is {name}</h1>
+      <h1 style={mystyles}>Hello my name is {name}</h1>
       <p>I'm from {city}</p>
       <p>My favorite hobby is {hobbies[2]}</p>
-      <p>Distance from Argatha to Rohngawi is {meterConvert(17)}m</p>
+      <p>60 + 7 = {addNumber(60, 7)}</p>
+      <h2>{y}</h2>
+      <button onClick={myfunc}>Click Me</button>
     </div>
   );
 }
 
-function meterConvert(km) {
-  return km * 1000
+function addNumber(num1, num2) {
+  return num1 + num2;
 }
 
 export default App;
