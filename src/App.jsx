ad4a5c5ddce169b37f87e84fc0281f714780f555
Fadri <fadriasliarga@gmail.com>
2026-09-17 12:51:43 +0700

    learn handle add

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,45 @@
 import { useState } from "react";
 
 function App() {
+  const [item, setItem] = useState([
+    { id: 1, name: "Learn usestate" },
+    { id: 2, name: "Build simple CRUD" },
+  ]);
 
-  const [items, setItems] = useState([
-    {id: 1, name: "Learn UseState"},
-    {id: 2, name: "Create Simple CRUD"}
-  ])
+  const [newItem, setNewItem] = useState("");
+
+  function handleAdd() {
+    if (newItem.trim() === "") return;
+
+    const newData = {
+      id: Date.now(),
+      name: newItem,
+    };
+
+    setItem([...item, newData]);
+    setNewItem("");
+  }
 
   return (
     <div>
       <h1>Simple CRUD</h1>
+
+      <input 
+      type="text"
+      value={newItem}
+      onChange={(e) => setNewItem(e.target.value)}
+      placeholder="Write new item"
+      />
+
+      <button onClick={handleAdd}>Add</button>
+
       <ul>
-        {items.map((item) => (
+        {item.map((item) => (
           <li key={item.id}>{item.name}</li>
         ))}
       </ul>
     </div>
-  )
+  );
 }
 
 export default App;
