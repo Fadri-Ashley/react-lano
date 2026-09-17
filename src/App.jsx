a0a3557e9b63aaa9ac5d5e23c649b7337c0a266d
Fadri <fadriasliarga@gmail.com>
2026-09-17 09:55:50 +0700

    learn & implement UseState for build simple CRUD

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,22 @@
+import { useState } from "react";
+
 function App() {
+
+  const [items, setItems] = useState([
+    {id: 1, name: "Learn UseState"},
+    {id: 2, name: "Create Simple CRUD"}
+  ])
+
   return (
     <div>
-      <h1>Hello World</h1>
+      <h1>Simple CRUD</h1>
+      <ul>
+        {items.map((item) => (
+          <li key={item.id}>{item.name}</li>
+        ))}
+      </ul>
     </div>
-  );
+  )
 }
 
 export default App;
